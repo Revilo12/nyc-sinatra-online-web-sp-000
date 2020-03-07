57017a57823f8580b1d38f18819b3a0527ec9655
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-07 22:53:07 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,9 +5,24 @@ class FiguresController < ApplicationController
     erb :'figures/index'
   end
 
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @landmark_name = params[:landmark][:name]
+    @title_name = params[:title][:name]
+    if !params[:landmark][:name].empty?
+      if landmark = Landmark.fing_by(name: pa)
+
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :'figures/show'
   end
+
+
 
 end
