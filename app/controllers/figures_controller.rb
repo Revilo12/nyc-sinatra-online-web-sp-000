84efec61b8881ad61fd82f2d556f461e8b1981fc
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-07 23:19:44 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,29 +15,25 @@ class FiguresController < ApplicationController
     @figure = Figure.create(params[:figure])
     @landmark_name = params[:landmark][:name]
     @title_name = params[:title][:name]
-
     if !@landmark_name.empty?
       if !(landmark = Landmark.fing_by(name: @landmark_name))
         landmark = Landmark.create(name: @landmark_name)
       end
       @figure.landmarks << landmark
     end
+    if !@landmark_name.empty?
+      if !(landmark = Landmark.fing_by(name: @landmark_name))
+        landmark = Landmark.create(name: @landmark_name)
+      end
+      @figure.landmarks << landmark
+    end
+    @figure.save
+    redirect '/figures/#{@figure.id}'
   end
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :'figures/show'
-  end
-
-  def helpers
-    def check_set_input_name(instance, input_name, input_class)
-      if !input_name.empty?
-        if !(assigned_instance = send("#{input_class}.find_by(name: #{input_name})"))
-          assigned_instance = send("#{input_class}.create(name: #{input_name})")
-        end
-        send("instance.#{input_class.name.downcase}") << assigned_instance
-      end
-    end
   end
 
 end
