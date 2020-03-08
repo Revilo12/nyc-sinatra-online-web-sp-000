ff8907b1a58d0f36ca7b44509835dc11b643197b
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-08 00:20:22 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,6 +1,20 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+    @all = Landmark.all
+  end
+
   get '/landmarks/new' do
     erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    erb :'landmark/show'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
   end
 end
